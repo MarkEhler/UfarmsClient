4b3671b05ec9ce2abb42e3bc98685764cd25e749
Joseph McGreene <josephorianmcgreene@gmail.com>
2024-01-31 21:45:59 -0800

    Reset frontend for landing page

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Ufarms</h1>
+      <p>Let&apos;s Save the World.</p>
+    </div>
+  );
+}
